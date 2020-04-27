89530923e09f78c4a15bea5f1f9ef7fa620c2b06
devdevil <thevampir2lestat@gmail.com>
2020-04-27 14:05:37 +0900

    Build sties

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "http://devdevil1901.github.io/jekyll/update/welcome-to-jekyll/",
         "teaser": null
+      },{
+        "title": "Trump Insults CNN Reporter Who Challenged Him",
+        "excerpt":"Today shadowing. comment: Why did Trump congratulate himself after 40k people died? Let him explain. Trump: like yeah here we go CNN Reporter:(00:08) Mr. president. 22 more than 22 million Americans are currently unemployed yeah as a result of this. today we hit the grim milestone of more than 40,000...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://devdevil1901.github.io/jekyll/update/English-Study-Shadowing-covid-and-trumpjekyll/",
+        "teaser": null
       }]
